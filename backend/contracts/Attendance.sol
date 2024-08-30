480760d5bf980b4fdff8e42d700e075184985330
krishnendu-2003 <skrishnendu115@gmail.com>
2024-08-30 21:45:52 +0530

    Updated

--- backend/contracts/Attendance.sol
+++ backend/contracts/Attendance.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract Attendance {
-    // Event to be emitted when attendance is marked
     event AttendanceMarked(address indexed teacher, string[] studentNames);
 
-    // Mapping to store attendance
     mapping(address => string[]) private attendance;
 
-    // Function to mark attendance by submitting an array of student names
     function markAttendance(string[] memory studentNames) public {
         require(studentNames.length > 0, "No students provided");
 
@@ -17,7 +14,6 @@ contract Attendance {
         emit AttendanceMarked(msg.sender, studentNames);
     }
 
-    // Function to get the list of students marked by the caller
     function getAttendance() public view returns (string[] memory) {
         return attendance[msg.sender];
     }
