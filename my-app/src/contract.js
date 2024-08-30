e985aefc8a34d65694c672e4a38011d75d2a4e9b
krishnendu-2003 <skrishnendu115@gmail.com>
2024-08-30 18:26:01 +0530

    Updated

--- my-app/src/contract.js
+++ my-app/src/contract.js
@@ -3,13 +3,9 @@ import votingABI from './abi/Voting.json';
 
 const contractAddress = '0xaf1bc8ab0210848b38dfd602eef7e9634a5cbc0d'; 
 
-// Function to get the smart contract instance
 export const getContract = () => {
-  // Create a provider
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  // Get the signer from the provider
   const signer = provider.getSigner();
-  // Create and return a contract instance
   return new ethers.Contract(contractAddress, votingABI, signer);
 };
 
